2ec0e34c55d5641d3b372e9c49f5e5e3bcfdae68
daveringu <david.ringuet@polymtl.ca>
2020-09-17 21:33:07 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def get_first_part_of_name(name):
 
 
 def get_random_sentence(animals, adjectives, fruits):
-	animal_index = random.ran
+	animal_index = random.randrange(0)
     adjectives=adjectives[random]
     fruits=fruits[random]
     return "Aujourd’hui, j’ai vu un ",animals," s’emparer d’un panier ",adjectives," plein de ",fruits,"."
