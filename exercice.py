30fcad2fc05531bcb66208eaacb0d5c2c3b534ee
daveringu <david.ringuet@polymtl.ca>
2020-09-17 21:34:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,8 +23,8 @@ def get_first_part_of_name(name):
 
 def get_random_sentence(animals, adjectives, fruits):
 	animal_word = random.randrange(0,len(animals))
-    animal_word = random.randrange(0,len(animals))
-    animal_word = random.randrange(0,len(animals))
+    adjectif_word = random.randrange(0,len(animals))
+    fruit_word = random.randrange(0,len(s))
 
     adjectives=adjectives[random]
     fruits=fruits[random]
